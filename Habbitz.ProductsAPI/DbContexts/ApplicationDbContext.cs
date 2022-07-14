59f215485ed0a6c7b31bbb3d996af1e5bb2b1c84
anoldKupara <57469308+anoldKupara@users.noreply.github.com>
2022-07-14 12:06:15 +0200

    added migrations for seeding into DB

--- Habbitz.ProductsAPI/DbContexts/ApplicationDbContext.cs
+++ Habbitz.ProductsAPI/DbContexts/ApplicationDbContext.cs
@@ -18,7 +18,7 @@ namespace Habbitz.ProductsAPI.DbContexts
                 Name = "Samosa",
                 Price = 5,
                 Description = "A product made of flour and meat or potatoes",
-                ImageUrl = "",
+                ImageUrl = "https://drive.google.com/file/d/1lBr9BJIXaoc-aICwwhFjI4hu3gO4R_W4/view?usp=sharing",
                 CategoryName = "Appetizer"
             });
             modelBuilder.Entity<Product>().HasData(new Product
@@ -27,7 +27,7 @@ namespace Habbitz.ProductsAPI.DbContexts
                 Name = "Panner Tikka",
                 Price = 13.45,
                 Description = "To be added later",
-                ImageUrl ="",
+                ImageUrl = "https://drive.google.com/file/d/1PjE7OJWfOBQkAj-iwnF9hMD2sukfe8YV/view?usp=sharing",
                 CategoryName = "Appetizer"
             });
             modelBuilder.Entity<Product>().HasData(new Product
@@ -36,7 +36,7 @@ namespace Habbitz.ProductsAPI.DbContexts
                 Name = "Sweet Pie",
                 Price = 10.99,
                 Description = "To be added later",
-                ImageUrl = "",
+                ImageUrl = "https://drive.google.com/file/d/1H_jRrTvSAV-OXNe4NCMbYSlU87lMzAjd/view?usp=sharing",
                 CategoryName = "Dessert"
             });
             modelBuilder.Entity<Product>().HasData(new Product
@@ -45,7 +45,7 @@ namespace Habbitz.ProductsAPI.DbContexts
                 Name = "Ice Cream",
                 Price = 2.67,
                 Description = "To be added laterr",
-                ImageUrl = "",
+                ImageUrl = "https://drive.google.com/file/d/12UVuU89klgzU62yTyaE4NdKrflGM7uOn/view?usp=sharing",
                 CategoryName = "Dessert"
             });
             modelBuilder.Entity<Product>().HasData(new Product
@@ -54,7 +54,7 @@ namespace Habbitz.ProductsAPI.DbContexts
                 Name = "Gelato",
                 Price = 8.16,
                 Description = "A fancy dessert",
-                ImageUrl = "",
+                ImageUrl = "https://drive.google.com/file/d/12Z9dQ70racP7RMuVfZ51VP1mrCZlLsj9/view?usp=sharing",
                 CategoryName = "Dessert"
             });
         }
---
+++ Habbitz.ProductsAPI/Migrations/20220714100037_SeedProducts.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Habbitz.ProductsAPI.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryName", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Appetizer", "A product made of flour and meat or potatoes", "https://drive.google.com/file/d/1lBr9BJIXaoc-aICwwhFjI4hu3gO4R_W4/view?usp=sharing", "Samosa", 5.0 },
+                    { 2, "Appetizer", "To be added later", "https://drive.google.com/file/d/1PjE7OJWfOBQkAj-iwnF9hMD2sukfe8YV/view?usp=sharing", "Panner Tikka", 13.449999999999999 },
+                    { 3, "Dessert", "To be added later", "https://drive.google.com/file/d/1H_jRrTvSAV-OXNe4NCMbYSlU87lMzAjd/view?usp=sharing", "Sweet Pie", 10.99 },
+                    { 4, "Dessert", "To be added laterr", "https://drive.google.com/file/d/12UVuU89klgzU62yTyaE4NdKrflGM7uOn/view?usp=sharing", "Ice Cream", 2.6699999999999999 },
+                    { 5, "Dessert", "A fancy dessert", "https://drive.google.com/file/d/12Z9dQ70racP7RMuVfZ51VP1mrCZlLsj9/view?usp=sharing", "Gelato", 8.1600000000000001 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 5);
+        }
+    }
+}

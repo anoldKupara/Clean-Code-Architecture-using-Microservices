722a0f8753db7cc4c80f5ce84b4312745953325a
anoldKupara <57469308+anoldKupara@users.noreply.github.com>
2022-07-14 11:41:10 +0200

    seeded data into the Database

--- Habbitz.ProductsAPI/DbContexts/ApplicationDbContext.cs
+++ Habbitz.ProductsAPI/DbContexts/ApplicationDbContext.cs
@@ -9,5 +9,54 @@ namespace Habbitz.ProductsAPI.DbContexts
         {
         }
         public DbSet<Product> Products { get; set; }    
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name = "Samosa",
+                Price = 5,
+                Description = "A product made of flour and meat or potatoes",
+                ImageUrl = "",
+                CategoryName = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 2,
+                Name = "Panner Tikka",
+                Price = 13.45,
+                Description = "To be added later",
+                ImageUrl ="",
+                CategoryName = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 3,
+                Name = "Sweet Pie",
+                Price = 10.99,
+                Description = "To be added later",
+                ImageUrl = "",
+                CategoryName = "Dessert"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 4,
+                Name = "Ice Cream",
+                Price = 2.67,
+                Description = "To be added laterr",
+                ImageUrl = "",
+                CategoryName = "Dessert"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 5,
+                Name = "Gelato",
+                Price = 8.16,
+                Description = "A fancy dessert",
+                ImageUrl = "",
+                CategoryName = "Dessert"
+            });
+        }
     }
 }

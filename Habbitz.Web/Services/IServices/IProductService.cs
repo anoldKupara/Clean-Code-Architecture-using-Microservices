88dee449d05ab91615078d8bb780790cdb0744a8
anoldKupara <57469308+anoldKupara@users.noreply.github.com>
2022-07-14 12:30:06 +0200

    added IProductService interface

---
+++ Habbitz.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Habbitz.Web.Models;
+
+namespace Habbitz.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDto producto);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);   
+        Task DeleteProductAsync<T>(int id);
+    }
+}
